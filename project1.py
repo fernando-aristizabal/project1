264c305336708da172f40ea86be7a00edcd6470c
fernandoa123 <fernandoa.eng@gmail.com>
2018-09-21 18:48:44 -0400

    Add files via upload

--- project1.py
+++ project1.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.io import loadmat, whosmat
+from scipy.io import loadmat, whosmat, savemat
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
@@ -8,6 +8,29 @@ from pprint import pprint
 
 
 ## functions ##
+def preProcessData(inputMatFile,outputMatFile):
+	## load data ##
+	data=loadmat(inputMatFile)
+
+	## reshape data ##
+	X = np.reshape(data['Proj1Im'],(data['Proj1Im'].shape[0]*data['Proj1Im'].shape[1],data['Proj1Im'].shape[2]))
+	Y = data['Proj1ClassLabels'].ravel()
+
+	## fill in missing values ##
+	imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
+	imp.fit(X)
+	X = imp.transform(X)
+
+	## subset training/testing data by class
+	idx1 = [ Y == -1]
+	idx2 = [ Y == 1 ]
+
+	processedData = { 'X' : X, 'Y' : Y, 'idx1' : idx1, 'idx2' : idx2 }
+	savemat(outputMatFile,processedData)
+
+	return(X,Y,idx1,idx2)
+
+
 def Classify(TrainSet,TestSet,LabelsTrain):
 	
 	model = SVC()
@@ -22,49 +45,43 @@ def ScoreClassifer(TrueLabels,OutLabels):
 	PercCorrect = 100 * sum(TrueLabels==OutLabels) / len(TrueLabels)
 	return(PercCorrect)
 
-## load data ##
-data=loadmat('Proj1Data.mat')
-#pprint(whosmat('Proj1Data'))
 
-## reshape data ##
-X = np.reshape(data['Proj1Im'],(data['Proj1Im'].shape[0]*data['Proj1Im'].shape[1],data['Proj1Im'].shape[2]))
-Y = data['Proj1ClassLabels'].ravel()
+def RunAll(numberOfExperiments=2):
+	
+	X,Y,idx1,idx2 = preProcessData('Proj1Data.mat','Processed_Proj1Data.mat')
 
-## fill in missing values ##
-imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
-imp.fit(X)
-X = imp.transform(X)
+	algo = 2
+	datasets = 2
+	CPtrain = np.zeros((datasets,algo,numberOfExperiments))-1 # intialize to negative one
+	CPtest = np.zeros((datasets,algo,numberOfExperiments))-1 # intialize to negative one
 
-## subset training/testing data by class
-"""
-idxTrain1 = [Ytrain==-1]
-idxTrain2 = [Ytrain==1]
-idxTest1 = [Ytest==-1]
-idxTest2 = [Ytest==1]
-"""
-n = 2
-algo = 2
-CP = np.zeros((n,algo))-1 # intialize to negative one
+	for n in range(numberOfExperiments):
+		print("Experiment: {}".format(n+1))
+		for a in range(algo):
+			print("  Algorithm: {}".format(a+1))
+			for d in range(datasets):
+				print("    Dataset: {}".format(d+1))
+				
+				Xtrain, Xtest, Ytrain, Ytest = train_test_split(X,Y,train_size=0.75,stratify=Y)
+				
+				OutLabelsTrain,OutLabelsTest = Classify(Xtrain,Xtest,Ytrain)
+				
+				CPtrain[d,a,n] = ScoreClassifer(Ytrain,OutLabelsTrain)
+				CPtest[d,a,n] = ScoreClassifer(Ytest,OutLabelsTest)
 
-for e in range(n):
-	print("Experiment: {}".format(e+1))
-	for a in range(algo):
-		print("\tAlgorithm: {}".format(a+1))
-		Xtrain, Xtest, Ytrain, Ytest = train_test_split(X,Y,train_size=0.75,stratify=Y)
-		OutLabelsTrain,OutLabelsTest = Classify(Xtrain,Xtest,Ytrain)
-		CP[e,a] = ScoreClassifer(Ytrain,OutLabelsTrain)
+	meansTrain = np.around(np.mean(CPtrain,axis=2),decimals=1)
+	stdTrain = np.around(np.std(CPtrain,axis=2),decimals=1)
+	CPtrain = np.around(CPtrain,decimals=0)
 
-means = np.around(np.mean(CP,axis=0),decimals=1)
-std = np.around(np.std(CP,axis=0),decimals=1)
-CP = np.around(CP,decimals=0)
+	meansTest = np.around(np.mean(CPtest,axis=2),decimals=1)
+	stdTest = np.around(np.std(CPtest,axis=2),decimals=1)
+	CPtest = np.around(CPtest,decimals=0)
 
-print(CP)
-print(means)
-print(std)
+	print(meansTrain) ; print(stdTrain) #; print(CPtrain)
+	print(meansTrain) ; print(stdTrain) #; print(CPtrain) 
 
 
-
-
+RunAll(numberOfExperiments=2)
 
 
 
